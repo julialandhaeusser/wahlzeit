a861c81c7d8fed3e7da8bc4a97ee295e935d3a4d
Julia Landhäußer <julia@landhaeusser.de>
2019-11-18 01:53:10 +0100

    fix test suite

--- src/test/java/org/wahlzeit/model/CoordinateTestSuite.java
+++ src/test/java/org/wahlzeit/model/CoordinateTestSuite.java
@@ -5,6 +5,6 @@ import org.junit.runners.Suite;
 
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({CartesianCoordinate.class, SphericCoordinate.class})
+@Suite.SuiteClasses({CartesianCoordinateTest.class, SphericCoordinateTest.class})
 public class CoordinateTestSuite {
 }
